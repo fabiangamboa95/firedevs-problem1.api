cfbc3ff0c95f1c05c9a7dad33faa6f89aacf29e8
fabiangamboa95 <fabiangamboa95@gmail.com>
2021-08-24 16:56:39 -0400

    updated students model

--- src/models/students.model.ts
+++ src/models/students.model.ts
@@ -1,23 +1,29 @@
 // See http://docs.sequelizejs.com/en/latest/docs/models-definition/
 // for more of what you can do here.
-import { Sequelize, DataTypes, Model } from 'sequelize';
-import { Application } from '../declarations';
-import { HookReturn } from 'sequelize/types/lib/hooks';
+import { Sequelize, DataTypes, Model } from "sequelize";
+import { Application } from "../declarations";
+import { HookReturn } from "sequelize/types/lib/hooks";
 
 export default function (app: Application): typeof Model {
-  const sequelizeClient: Sequelize = app.get('sequelizeClient');
-  const students = sequelizeClient.define('students', {
-    text: {
-      type: DataTypes.STRING,
-      allowNull: false
+  const sequelizeClient: Sequelize = app.get("sequelizeClient");
+  const students = sequelizeClient.define(
+    "students",
+    {
+      edad: { type: DataTypes.INTEGER, allowNull: false },
+      sexo: { type: DataTypes.STRING, allowNull: false },
+      nombre: { type: DataTypes.STRING, allowNull: false },
+      email: { type: DataTypes.STRING, allowNull: false },
+      fechaNacimiento: { type: DataTypes.DATE, allowNull: false },
+      ciudadNacimiento: { type: DataTypes.STRING, allowNull: false },
+    },
+    {
+      hooks: {
+        beforeCount(options: any): HookReturn {
+          options.raw = true;
+        },
+      },
     }
-  }, {
-    hooks: {
-      beforeCount(options: any): HookReturn {
-        options.raw = true;
-      }
-    }
-  });
+  );
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   (students as any).associate = function (models: any): void {
